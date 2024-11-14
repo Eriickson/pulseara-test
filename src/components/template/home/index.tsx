f786e87c1f5b41b19271244be3cab1dd5a85cb33
Erickson Manuel Holguin <erickson01d@gmail.com>
2024-11-13 22:37:16 -0400

    Refactor HomePageTemplate to improve layout structure by consolidating nested boxes for better readability

--- src/components/template/home/index.tsx
+++ src/components/template/home/index.tsx
@@ -9,17 +9,15 @@ export const HomePageTemplate = () => {
     <Box minH="100dvh" backgroundColor="#EDF3F1">
       <Container maxW="1440px" paddingX="88px" paddingTop="99px">
         <Box>
-          <Box>
-            <Box mb="35px">
-              <Heading w="max-content" fontSize="25px" fontWeight="semibold" lineHeight="29.35px">
-                Procedimientos
-              </Heading>
-            </Box>
-            <Box mb="35px">
-              <ProceduresList />
-            </Box>
-            <EditProcedures />
+          <Box mb="35px">
+            <Heading w="max-content" fontSize="25px" fontWeight="semibold" lineHeight="29.35px">
+              Procedimientos
+            </Heading>
           </Box>
+          <Box mb="35px">
+            <ProceduresList />
+          </Box>
+          <EditProcedures />
         </Box>
       </Container>
     </Box>
