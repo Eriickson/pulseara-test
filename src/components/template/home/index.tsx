81874ae927cae0b32d97cb6a32878ffca5af9c0f
Erickson Manuel Holguin <erickson01d@gmail.com>
2024-11-14 15:24:57 -0400

    Refactor HomePageTemplate layout to use Flex for improved structure; add empty state illustration and message for no data

--- src/components/template/home/index.tsx
+++ src/components/template/home/index.tsx
@@ -1,32 +1,38 @@
 import React from "react";
 
-import { Box, Container, Heading, Stack } from "@chakra-ui/react";
+import { Box, Center, Container, Flex, Heading, Image, Stack, Text } from "@chakra-ui/react";
 
 import { EditProcedures } from "./edit-procedures";
 import { ProceduresList } from "./procedures-list";
+import { useProcedure } from "../../../context/procedure";
 
 export const HomePageTemplate = () => {
+  const { procedures } = useProcedure();
+
   return (
-    <Box minH="100vh" backgroundColor="#EDF3F1">
-      <Container maxW="1440px" paddingX={{ base: "4", md: "88px" }} paddingTop={{ base: "4", md: "99px" }}>
-        <Box>
-          <Stack alignItems="flex-start" gapY={{ base: "4", md: "35px" }}>
-            <Box>
-              <Heading
-                w="max-content"
-                fontSize={{ base: "2xl", md: "25px" }}
-                fontWeight="semibold"
-                lineHeight="29.35px"
-                color="black"
-              >
-                Procedimientos
-              </Heading>
-            </Box>
-            <ProceduresList />
+    <Flex minH="100vh" backgroundColor="#EDF3F1">
+      <Container maxW="1440px" paddingX={{ base: "4", md: "88px" }} paddingY={{ base: "4", md: "99px" }} flex="1">
+        <Stack h="full" alignItems="flex-start" gapY={{ base: "4", md: "35px" }}>
+          <Box>
+            <Heading
+              w="max-content"
+              fontSize={{ base: "2xl", md: "25px" }}
+              fontWeight="semibold"
+              lineHeight="29.35px"
+              color="black"
+            >
+              Procedimientos
+            </Heading>
+          </Box>
+          <Center flexDirection="column" w="full" spaceY="20px" flex="1">
+            <Image src="/assets/empty-illustration.svg" />
+            <Text fontFamily="Roboto">No hay datos que mostrar</Text>
             <EditProcedures />
-          </Stack>
-        </Box>
+          </Center>
+          {/* <ProceduresList />
+            <EditProcedures /> */}
+        </Stack>
       </Container>
-    </Box>
+    </Flex>
   );
 };
